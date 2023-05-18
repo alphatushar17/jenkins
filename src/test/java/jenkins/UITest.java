0010433241c88927239ed5924ac79f55b38c62d1
admin <sharmatushar334@gmail.com>
2023-05-18 15:27:22 +0530

    second commit

--- src/test/java/jenkins/UITest.java
+++ src/test/java/jenkins/UITest.java
@@ -3,14 +3,29 @@ package jenkins;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-public class UITest {
+public class UITest
+{
+    @Parameters("Browser")
     @Test
-    public void startBrowser(){
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver=new ChromeDriver();
+    public void startBrowser(String browserName)
+    {
+        System.out.println("Parameter value is" + browserName);
+        WebDriver driver =null;
+
+        if (browserName.contains("Chrome")) {
+            WebDriverManager.chromedriver().setup();
+             driver=new ChromeDriver();
+        }
+        else if (browserName.contains("Edge"))
+        {
+            WebDriverManager.edgedriver().setup();
+            driver=new EdgeDriver();
+        }
         driver.manage().window().maximize();
         driver.get("https://opensource-demo.orangehrmlive.com/");
         Assert.assertTrue(driver.getTitle().contains("Orange"),"Title does not match");
